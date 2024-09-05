edd3d31aafafe489248723bbf4feabed54ebd1c3
tuongtran1106 <tuongtran1106@gmail.com>
2024-09-05 16:41:03 +0700

    Using Lazy loading & Routing to Lazy-load Services

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,27 +1,37 @@
 import { ResolveFn, Routes } from '@angular/router';
 
-import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import {
+  NewTaskComponent,
+  canLeaveEditPage,
+} from '../tasks/new-task/new-task.component';
 import { TaskComponent } from '../tasks/task/task.component';
 import { resolveUserTasks } from '../tasks/tasks.component';
+import { TasksService } from '../tasks/tasks.service';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TaskComponent,
-    // loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TaskComponent,
+        // loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
   },
 ];

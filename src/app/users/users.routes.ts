658fa24040ae0b2d1e20d18c520e5fa86ec6fdf3
tuongtran1106 <tuongtran1106@gmail.com>
2024-09-05 16:34:02 +0700

    Update resolver back to component

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,31 +1,8 @@
 import { ResolveFn, Routes } from '@angular/router';
 
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
-import { Task } from '../tasks/task/task.model';
-import { inject } from '@angular/core';
-import { TasksService } from '../tasks/tasks.service';
 import { TaskComponent } from '../tasks/task/task.component';
-
-export const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-
-  return tasks.length ? tasks : [];
-};
+import { resolveUserTasks } from '../tasks/tasks.component';
 
 export const routes: Routes = [
   {

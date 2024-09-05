8d9f186a7a29ef60b18093ca73e0d16667ac0970
tuongtran1106 <tuongtran1106@gmail.com>
2024-09-05 16:27:27 +0700

    Implement route-based lazy loading

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,22 @@
+import {
+  Component,
+  inject,
+  input,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { TaskComponent } from './task/task.component';
+import { Task } from './task/task.model';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+  imports: [TaskComponent, RouterLink],
+})
+export class TasksComponent {
+  userTasks = input.required<Task[]>();
+  userId = input.required<string>();
+  order = input<'asc' | 'desc' | undefined>();
+}
---
+++ src/app/users/users.routes.ts
@@ -0,0 +1,48 @@
+import { ResolveFn, Routes } from '@angular/router';
+
+import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import { Task } from '../tasks/task/task.model';
+import { inject } from '@angular/core';
+import { TasksService } from '../tasks/tasks.service';
+
+export const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks', // <your-domain>/users/<uid>/tasks
+    loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
+    runGuardsAndResolvers: 'always',
+    resolve: {
+      userTasks: resolveUserTasks,
+    },
+  },
+  {
+    path: 'tasks/new',
+    component: NewTaskComponent,
+    canDeactivate: [canLeaveEditPage]
+  },
+];
